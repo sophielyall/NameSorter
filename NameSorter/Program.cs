9f09407624b41ac7e626d95b820b28f8e78fe2ea
sophielyall <sophie.lyall@outlook.com>
2021-12-20 14:12:10 +1100

    Add files via upload

---
+++ NameSorter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NameSorter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   //read names from file
+            FileReader reader = new FileReader(args[0]);
+            List<String> allNames = reader.readNames();
+            List<Name> unsortedNames = new List<Name>();
+            foreach (string line in allNames)
+            {
+
+                NameConverter converter = new NameConverter();  
+                //add name to 
+                Name aName = converter.splitToName(line);
+                unsortedNames.Add(aName);
+
+            }
+            NameSorter sorter = new NameSorter();
+            //sort the names in the file
+            var sortedNames = sorter.sortName(unsortedNames);
+           
+            Console.WriteLine("Sorted List: ");
+            FileWriter writer = new FileWriter("sorted-name-list.txt");
+            writer.WriteNames(sortedNames);
+        }
+
+
+    }
+}
+
