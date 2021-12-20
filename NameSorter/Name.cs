8a2ae31f474e2397c332d54fdc65a1b58d031615
sophielyall <sophie.lyall@outlook.com>
2021-12-20 11:12:02 +1100

    Add files via upload

---
+++ NameSorter/Name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace NameSorter
+{
+    public class Name
+    {
+
+        public string firstName { get; }
+        public string lastName { get; }
+        public Name(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string getFullName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        
+    }
+}
